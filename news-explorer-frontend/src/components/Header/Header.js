148ed5f7003fd7b824f52a23cf85151c3e94549e
Shayenka Alvarado <shayenkalvarado@gmail.com>
2024-03-18 15:54:47 -0600

    arreglar icono menu

--- news-explorer-frontend/src/components/Header/Header.js
+++ news-explorer-frontend/src/components/Header/Header.js
@@ -4,7 +4,8 @@ import useUserContext from "../Hooks/useUserContext";
 import Logout from "../../images/logout.svg";
 import LogoutBlack from "../../images/logout-black.svg";
 import MenuMovile from "../../images/menu-movile.svg";
-import closePopUp from "../../images/close.svg";
+import closeMenu from "../../images/closeMenu.svg";
+import MenuMovileSavedNews from "../../images/menu-movile-savedNews.svg"
 
 function Header({
   handleLoginPopUp,
@@ -17,35 +18,53 @@ function Header({
 
   const handleClick = () => {
     setOpen(!open);
-  }
- 
+  };
 
   return (
     <header className="header">
       {isLoggedIn ? (
         <>
-        <h2
+          <h2
             className={`header__text ${
-              isSavedNewsClicked ? "header__text_SavedNews" : ""
+              isSavedNewsClicked ? "header__text_movile_SavedNews" : ""
             }`}
           >
             {" "}
             NewsExplorer{" "}
           </h2>
-          <button onClick={handleClick} className="header__button header__button_nav">
-            <img src={MenuMovile} alt="Menú" />
+          <button
+            onClick={handleClick}
+            className="header__button header__button_nav"
+          >
+            <img
+    src={isSavedNewsClicked ? MenuMovileSavedNews : MenuMovile}
+    alt="Menú"
+  />
           </button>
-          {/* <div className={`header__container-texts-movile ${
-  open ? (isSavedNewsClicked ? "header__container-texts-movile_open-SavedNews" : "header__container-texts-movile_open") : ""
-}`}> */}
 
-<div className={`header__container-texts-movile ${isSavedNewsClicked ? 'header__container-texts-movile-savedNews' : ''} ${ open ? (isSavedNewsClicked ? "header__container-texts-movile_open-SavedNews" : "header__container-texts-movile_open") : ""
-}`}>
-          <h2 className="header__text"> NewsExplorer </h2>
-          <button onClick={handleClick} className="header__button">
-          <img src={closePopUp} alt="Icono de una X para cerrar el menú." />
-          </button>
-          
+          <div
+            className={`header__container-texts-movile ${
+              isSavedNewsClicked
+                ? "header__container-texts-movile-savedNews"
+                : ""
+            } ${
+              open
+                ? isSavedNewsClicked
+                  ? "header__container-texts-movile_open-SavedNews"
+                  : "header__container-texts-movile_open"
+                : ""
+            }`}
+          >
+         <div className="header__container_movile">
+         <h2 className={`header__text_movile ${
+                isSavedNewsClicked ? "header__text_movile_SavedNews" : ""
+              }`}>NewsExplorer</h2>
+            {/* <h2 className="header__text_movile"> NewsExplorer </h2> */}
+            <button onClick={handleClick} className="header__button">
+              <img src={closeMenu} alt="Icono de una X para cerrar el menú." />
+            </button>
+          </div>
+          <div className="header__container__links_movile">
             <Link
               to="/"
               className={`header__link_text ${
@@ -59,41 +78,42 @@ function Header({
               Inicio
             </Link>
             {/* <div className="header__container_SavedNews"> */}
-              <Link
-                to="/saved-news"
-                className={`header__link_text ${
-                  isSavedNewsClicked ? "header__link_text_SavedNews" : ""
-                }`}
-                style={{ textDecoration: "none" }}
-                onClick={() => {
-                  onSavedNewsClick();
-                }}
-              >
-                Artículos guardados
-              </Link>
-              <Link
-                to="/"
-                className={`header__link_text-border ${
-                  isSavedNewsClicked ? "header__link_text-border_SavedNews" : ""
-                }`}
-                style={{
-                  textDecoration: "none",
-                  padding: "1px 20px",
-                  borderRadius: "30px",
-                }}
-                onClick={() => {
-                  handleLogout();
-                  handleSavedNewsClick();
-                }}
-              >
-                {currentUser && currentUser.name && <>{currentUser.name}</>}
-                <img
-                  className="header__icon-logout"
-                  src={isSavedNewsClicked ? LogoutBlack : Logout}
-                  alt="Icono de logout"
-                />
-              </Link>
-          </div> 
+            <Link
+              to="/saved-news"
+              className={`header__link_text ${
+                isSavedNewsClicked ? "header__link_text_SavedNews" : ""
+              }`}
+              style={{ textDecoration: "none" }}
+              onClick={() => {
+                onSavedNewsClick();
+              }}
+            >
+              Artículos guardados
+            </Link>
+            <Link
+              to="/"
+              className={`header__link_text-border ${
+                isSavedNewsClicked ? "header__link_text-border_SavedNews" : ""
+              }`}
+              style={{
+                textDecoration: "none",
+                padding: "1px 20px",
+                borderRadius: "30px",
+              }}
+              onClick={() => {
+                handleLogout();
+                handleSavedNewsClick();
+              }}
+            >
+              {currentUser && currentUser.name && <>{currentUser.name}</>}
+              <img
+                className="header__icon-logout"
+                src={isSavedNewsClicked ? LogoutBlack : Logout}
+                alt="Icono de logout"
+              />
+            </Link>
+            </div>
+          </div>
           {/* <h2
             className={`header__text ${
               isSavedNewsClicked ? "header__text_SavedNews" : ""
@@ -117,58 +137,65 @@ function Header({
               Inicio
             </Link>
             {/* <div className="header__container_SavedNews"> */}
-              <Link
-                to="/saved-news"
-                className={`header__link_text ${
-                  isSavedNewsClicked ? "header__link_text_SavedNews" : ""
-                }`}
-                style={{ textDecoration: "none" }}
-                onClick={() => {
-                  onSavedNewsClick();
-                }}
-              >
-                Artículos guardados
-              </Link>
-              <Link
-                to="/"
-                className={`header__link_text-border ${
-                  isSavedNewsClicked ? "header__link_text-border_SavedNews" : ""
-                }`}
-                style={{
-                  textDecoration: "none",
-                  padding: "1px 20px",
-                  borderRadius: "30px",
-                }}
-                onClick={() => {
-                  handleLogout();
-                  handleSavedNewsClick();
-                }}
-              >
-                {currentUser && currentUser.name && <>{currentUser.name}</>}
-                <img
-                  className="header__icon-logout"
-                  src={isSavedNewsClicked ? LogoutBlack : Logout}
-                  alt="Icono de logout"
-                />
-              </Link>
+            <Link
+              to="/saved-news"
+              className={`header__link_text ${
+                isSavedNewsClicked ? "header__link_text_SavedNews" : ""
+              }`}
+              style={{ textDecoration: "none" }}
+              onClick={() => {
+                onSavedNewsClick();
+              }}
+            >
+              Artículos guardados
+            </Link>
+            <Link
+              to="/"
+              className={`header__link_text-border ${
+                isSavedNewsClicked ? "header__link_text-border_SavedNews" : ""
+              }`}
+              style={{
+                textDecoration: "none",
+                padding: "1px 20px",
+                borderRadius: "30px",
+              }}
+              onClick={() => {
+                handleLogout();
+                handleSavedNewsClick();
+              }}
+            >
+              {currentUser && currentUser.name && <>{currentUser.name}</>}
+              <img
+                className="header__icon-logout"
+                src={isSavedNewsClicked ? LogoutBlack : Logout}
+                alt="Icono de logout"
+              />
+            </Link>
             {/* </div> */}
           </div>
         </>
       ) : (
         <>
           <h2 className="header__text"> NewsExplorer </h2>
-          <button onClick={handleClick} className="header__button header__button_nav">
+          <button
+            onClick={handleClick}
+            className="header__button header__button_nav"
+          >
             <img src={MenuMovile} alt="Menú" />
           </button>
-          <div className={`header__container-texts-movile ${
-                  open ? `header__container-texts-movile_open` : ""
-                }`}> 
-
-          <h2 className="header__text"> NewsExplorer </h2>
-          <button onClick={handleClick} className="header__button">
-          <img src={closePopUp} alt="Icono de una X para cerrar el menú." />
-          </button>
-          <Link
+          <div
+            className={`header__container-texts-movile ${
+              open ? `header__container-texts-movile_open` : ""
+            }`}
+          >
+            <div className="header__container_movile">
+            <h2 className="header__text_movile"> NewsExplorer </h2>
+            <button onClick={handleClick} className="header__button">
+              <img src={closeMenu} alt="Icono de una X para cerrar el menú." />
+            </button>
+            </div>
+            <div className="header__container__links_movile">
+            <Link
               to="/"
               className="header__link_text"
               style={{ textDecoration: "none" }}
@@ -190,6 +217,7 @@ function Header({
             >
               Iniciar sesión
             </Link>
+            </div>
           </div>
           <div className="header__container-texts-desktop">
             <Link
@@ -222,4 +250,3 @@ function Header({
 }
 
 export default Header;
-
